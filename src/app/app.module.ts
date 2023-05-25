5eb7b5f3cade57981287d6b6222599750c81d7e7
Luis Angel <luis.flores@durango.gob.mx>
2023-05-25 13:30:56 -0600

    Uso de modulos manuales y encapsulacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent,
   ],
   imports: [ // Si termina el archivo con Module, aqui se importa
     BrowserModule,
     CounterModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
